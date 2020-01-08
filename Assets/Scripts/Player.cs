4eaee7177ff1ae86dd59c85d66f801efb7a53703
Patrick G <Patchtheace@gmail.com>
2020-01-08 13:08:03 -0600

    Create the Player: Challenge: User Input
    Created input detection and basic movement for player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, -2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get left and right key press
+        float _Horizontal = Input.GetAxis("Horizontal");
+        //get up and down key press
+        float _Vertical = Input.GetAxis("Vertical");
+        //move player at _speed * time.deltatime
+        transform.Translate(new Vector3(_Horizontal, _Vertical, 0) * _speed * Time.deltaTime);
+
+
+
+
+    }
+}
