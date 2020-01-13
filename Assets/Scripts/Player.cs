c3523b7bf653fc1f032547be11ffdaf781457e38
Patrick G <Patchtheace@gmail.com>
2020-01-13 17:57:33 -0600

    Shooting: Challenge: Laser Behavior
    Coded Laser behavior (I had a lot of fun with this)
    Increased Game area to allow for better use of new laser behavior
    Note: may need to rename lasers to torpedoes.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -33,15 +33,15 @@ public class Player : MonoBehaviour
     void Calculatemovment()
     {
 
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.4f, 1.5f), 0);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -9.1f, 1.5f), 0);
 
-        if (transform.position.x >= 12f)
+        if (transform.position.x >= 20.8f)
         {
-            transform.position = new Vector3(-11.9f, transform.position.y, 0);
+            transform.position = new Vector3(-20.5f, transform.position.y, 0);
         }
-        else if (transform.position.x <= -12f)
+        else if (transform.position.x <= -20.8f)
         {
-            transform.position = new Vector3(11.9f, transform.position.y, 0);
+            transform.position = new Vector3(20.5f, transform.position.y, 0);
         }
 
         transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * _speed * Time.deltaTime);
