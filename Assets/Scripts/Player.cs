6ee496bde2f1ad5f518b96c212abaac285181815
Patrick G <Patchtheace@gmail.com>
2020-01-13 17:25:07 -0600

    Shooting: instantiate Laser
    Created Laser Prefab, Created code to spawn laser when player presses "Space key", Created Laser Script,  created Laser_Mat.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 4f;
+    [SerializeField]
+    private GameObject _laserPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,15 @@ public class Player : MonoBehaviour
     void Update()
     {
         Calculatemovment();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
+
+        //if space is pressed
+        //Instantiate prefab
+
     }
 
     void Calculatemovment()
