670a2246b1e019fee0e1dbd84472833a0315c4f9
Patrick G <Patchtheace@gmail.com>
2020-01-13 17:57:33 -0600

    Shooting: Challenge: Laser Behavior
    Coded Laser behavior (I had a lot of fun with this)
    Note: may need to rename lasers to torpedoes.

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+    [SerializeField]
+    private float _lazeroffsetdistance = 3.0f;
+    [SerializeField]
+    private float _speed;
+    private float _playerPositionY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerPositionY = GameObject.Find("Player").transform.position.y;
+
+        _speed = 2.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movementcontroller();
+    }
+    void Movementcontroller()
+    {
+        float lazerboost = _playerPositionY + _lazeroffsetdistance;
+
+        if (transform.position.y >= lazerboost && _speed != 15)
+        {
+            _speed = 12.0f;
+        }
+
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
     }
 }
+
